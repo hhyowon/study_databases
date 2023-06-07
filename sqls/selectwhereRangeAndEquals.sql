8b81266a7ebe07e15dc11c62690504cabd3145f1
hhyowon <hyowon0605@gmail.com>
2023-06-07 15:45:53 +0900

    good

--- sqls/selectwhereRangeAndEquals.sql
+++ sqls/selectwhereRangeAndEquals.sql
@@ -1,6 +1,6 @@
 SELECT * 
 FROM Customers
-WHERE City='London' AND CustomerID BETWEEN 50 AND 89
+WHERE City='London' AND CustomerID BETWEEN 50 AND 89;
 
 /*
  https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_where
